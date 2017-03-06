4af46e1c8ba2d7f46309e33e098558ad9ba28450
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-03-06 08:30:31 +0100

    working mongodb connection

--- src/main/java/org/currs/model/Repository.java
+++ src/main/java/org/currs/model/Repository.java
@@ -1,13 +1,51 @@
 package org.currs.model;
 
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+
 /**
  * Data repository
  */
 public class Repository implements IRepository {
 
+    private final int DB_PORT = 27017;
+    private final String DB_ADDRESS = "localhost";
+    private final String DB_NAME = "currs";
+
+    private MongoClient dbClient;
+    private MongoDatabase db;
+
+    public Repository() {
+    }
+
+    public void Connect() {
+        if (dbClient == null)
+            Disconnect();
+
+        dbClient = new MongoClient(DB_ADDRESS, DB_PORT);
+        db = dbClient.getDatabase(DB_NAME);
+    }
+
+    public void Disconnect() {
+        if (dbClient != null) {
+            dbClient.close();
+            dbClient = null;
+            db = null;
+        }
+    }
+
     @Override
     public String[] getAvailableCurrencies() {
-        return new String[0];
+        if (db == null)
+            return new String[0];
+
+        ArrayList<String> result = new ArrayList<String>();
+        db.listCollectionNames().forEach((Block<String>) e -> result.add(e));
+
+        return result.toArray(new String[0]);
     }
 
     @Override
--- src/main/java/org/currs/model/RepositoryFactory.java
+++ src/main/java/org/currs/model/RepositoryFactory.java
@@ -7,15 +7,21 @@ import org.glassfish.hk2.api.Factory;
  */
 public class RepositoryFactory implements Factory<IRepository> {
 
+    private Repository repository;
+
     public RepositoryFactory() {
+        repository = new Repository();
     }
 
     @Override
     public IRepository provide() {
-       return new Repository();
+       repository.Connect();
+       return repository;
     }
 
     @Override
     public void dispose(IRepository r) {
+        if (repository != null)
+            repository.Disconnect();
     }
 }
